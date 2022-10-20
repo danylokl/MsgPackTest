6608fc41c5f895de52ee68419e80cb13c7213aa2
danylokl <user>
2022-10-20 11:07:36 +0300

    Add project files.

---
+++ MsgPackTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using MessagePack;
+namespace MsgPackTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var user = new User
+            {
+                Id = 0,
+                Age = 18,
+                Name = "Jhon",
+                PhoneNumbers = new List<string> { "13849739","13295744" }
+            };
+
+            byte[] userMsgPack = MessagePackSerializer.Serialize(user);
+
+            var desirialazied = MessagePackSerializer.Deserialize<User>(userMsgPack);
+            var value = desirialazied.PhoneNumbers.FirstOrDefault();
+
+            Console.WriteLine(value);
+        }
+    }
+}
---
+++ MsgPackTest/User.cs
@@ -0,0 +1,23 @@
+﻿using MessagePack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MsgPackTest
+{
+    [MessagePackObject]
+    public class User
+    {
+        [Key(0)]
+        public int Id { get; set; }
+        [Key(1)]
+        public string Name { get; set; }
+        [Key(2)]
+        public int Age { get; set; }
+        [Key(3)]
+        public  List<string> PhoneNumbers { get; set; }
+
+    }
+}
